6741c0550df75cd1e966da6894eb582e75f5b461
adamhinton <adam.d.hinton@gmail.com>
2022-05-14 23:06:27 -0400

    notes on utility of DS

--- app.js
+++ app.js
@@ -3,3 +3,5 @@
 // dS are ollections of values, relationships amongst them, and the fxns or operations that can be applied to the data
 // adding, removing, push, pop
 // some ds are hihgly specialized, while others like arrays are more generally used
+// they all do the same thing: Storing data. But how you treat and act upon them is very different.
+// As you get more sophisiticated in your tasks, simple arrays and objects just don't cut it anymore.
