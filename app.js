92bb06b5b7fb502f2f08716ef64e1d3461b35aa9
adamhinton <adam.d.hinton@gmail.com>
2022-05-14 23:17:35 -0400

    objectives

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // we'll take advantae of es2015 classes for this. BUilding our own DS.
+
+// What's a class?
+// blueprint for creating objects with pre-def props and methods
+
+// objs:
+// understand classes
+// understand how js impl idea of classes
+//use es2015 to imp DS
