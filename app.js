1073f4ead580755780fe684bdbc9dcbf16454a23
adamhinton <adam.d.hinton@gmail.com>
2022-05-14 23:04:31 -0400

    basic notes on DS

---
+++ app.js
@@ -0,0 +1,5 @@
+// General notes about DS:
+
+// dS are ollections of values, relationships amongst them, and the fxns or operations that can be applied to the data
+// adding, removing, push, pop
+// some ds are hihgly specialized, while others like arrays are more generally used
