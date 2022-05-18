44d9d4670dd42bf7679db16032f3c86e56b5a027
adamhinton <adam.d.hinton@gmail.com>
2022-05-18 00:56:28 -0400

    singly linked lists notes

---
+++ app.js
@@ -0,0 +1,7 @@
+// SINGLY LINKED LISTS
+// We will be building our own SLL from scratch. Yay!
+
+// What is a linked list?
+// ordered DS that stores any value you want
+// in array, each is mapped with a number. But in a LL, there are nodes, and each node has a value, and a pointer to another node or null
+// So, no indexes
