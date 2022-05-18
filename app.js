19b4e1f8daf560dd106ac25923353622ce1a1a22
adamhinton <adam.d.hinton@gmail.com>
2022-05-18 00:57:26 -0400

    more LL notes. Length value pointer

--- app.js
+++ app.js
@@ -5,3 +5,7 @@
 // ordered DS that stores any value you want
 // in array, each is mapped with a number. But in a LL, there are nodes, and each node has a value, and a pointer to another node or null
 // So, no indexes
+// We call each element a node
+// its pointer points to another node
+// Head is first, tail is the end.
+// also keep track of length of LL
