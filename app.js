c4057b4c057f206df8116509157e3441abfb1034
adamhinton <adam.d.hinton@gmail.com>
2022-05-18 18:35:50 -0400

    refactor my push mtd

--- app.js
+++ app.js
@@ -18,9 +18,13 @@ class SinglyLinkedList {
     const newNode = new Node(val);
     if (!this.head) {
       this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    this.tail = newNode;
     this.length++;
+    return this;
   }
 }
 
