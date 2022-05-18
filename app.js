4619c4d4901362c83325a5708fb1581142e9361c
adamhinton <adam.d.hinton@gmail.com>
2022-05-18 18:20:13 -0400

    delete unneeded code

--- app.js
+++ app.js
@@ -8,11 +8,7 @@ class Node {
   }
 }
 
-const first = new Node("Hi");
-first.next = new Node("there");
-first.next.next = new Node("how");
-first.next.next.next = new Node("are");
-
-console.log("first:", first);
-
-console.log("blah blah blah");
+// const first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
